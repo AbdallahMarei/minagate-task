6591f0a378e4743cc73d16dfa9de6e08611f8ec1
Abdallah Marei <90200082+AbdallahMarei@users.noreply.github.com>
2022-01-23 16:51:14 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./components/navbar/navbar"
+import HomePage from './pages/home-page/home-page';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
